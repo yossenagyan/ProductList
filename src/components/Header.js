5398fddb0ab0276a15c9b5102d240b7794410a51
yossenagyan <ynagyan@gmail.com>
2020-09-06 10:06:25 +0700

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: "center",
+        height: 60,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        marginBottom: 10
+    },
+    text: {
+        fontSize: 25,
+        color: "white"
+    }
+})
 
 class Header extends Component {
   render() {
     return (
-      <View>
-        <Text>Header</Text>
+      <View style={style.container}>
+        <Text style={style.text}>Header</Text>
       </View>
     )
   }
