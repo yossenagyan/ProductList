781abaad56fd4e7d9510ce8698570815493a3771
yossenagyan <ynagyan@gmail.com>
2020-09-22 18:47:54 +0700

    ProductList

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native'
 
 import Header from './src/components/Header'
+import ProductList from './src/components/ProductList'
+
 
 class App extends Component {
   render() {
     return (
       <View>
         <Header title="Product List"/>
-        <Text>Hello world</Text>
+        <ProductList />
       </View>
     )
   }
---
+++ src/components/ProductList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Product List
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default ProductList;
